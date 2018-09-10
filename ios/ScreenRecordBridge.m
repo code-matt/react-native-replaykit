5e66b1bffacff898b4c67bb6b25b0180f86693a9
Matthew Thompson <mthompson@ADKs-MacBook-Pro-2.local>
2018-09-09 22:06:00 -0400

    adds empty bridging headers

---
+++ ios/ScreenRecordBridge.m
@@ -0,0 +1,7 @@
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(CalendarManager, NSObject)
+
+// RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(nonnull NSNumber *)date)
+
+@end
